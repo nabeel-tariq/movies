f948adc4003f67709127d942b39e51b47b42bb25
nabeel <nabeeltariq355@gmail.com>
2018-09-02 18:11:24 +0500

    rack core implement

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,14 @@ module MoviesProject
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.middleware.insert_before 0, Rack::Corsdo
+      allow do
+        origins '*'
+        resource '*', 
+        headers: :any, 
+        methods: [ :get, :post, :put, :patch, :delete, :options, :head]
+      end
+    end
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
